d75119c26464e2bdc6d1e8dd8d6f4e28e5062471
Joshua Ocansey <joshocan@gmail.com>
2026-02-06 12:25:22 +0100

    Add evaluation package for the program

---
+++ fame/evaluation/__init__.py
@@ -0,0 +1,9 @@
+from .context_stats import ContextStats, analyze_context_usage, analyze_prompt_usage, estimate_tokens, extract_prompt_sources
+
+__all__ = [
+    "ContextStats",
+    "analyze_context_usage",
+    "analyze_prompt_usage",
+    "estimate_tokens",
+    "extract_prompt_sources",
+]
