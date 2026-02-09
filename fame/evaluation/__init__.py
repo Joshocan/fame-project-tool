34133f06b6ae297571a589435eba50fe727bba14
Joshua Ocansey <joshocan@gmail.com>
2026-02-09 07:34:45 +0100

    Fix export timer helpers from evaluation to unblock pipelines

--- fame/evaluation/__init__.py
+++ fame/evaluation/__init__.py
@@ -8,6 +8,7 @@ Designed to be extended (structure, constraints, traceability, etc.).
 
 from .coverage import CoverageEvaluator, CoverageConfig, coverage_score
 from .wellformed import validate_feature_model, WellFormedResult
+from .duration import start_timer, elapsed_seconds
 
 __all__ = [
     "CoverageEvaluator",
@@ -15,4 +16,6 @@ __all__ = [
     "coverage_score",
     "validate_feature_model",
     "WellFormedResult",
+    "start_timer",
+    "elapsed_seconds",
 ]
